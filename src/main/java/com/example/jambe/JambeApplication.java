46d7650e137caa9db2a6ca30d39a53fed03ee5b6
찬 <cksgnlcjswoo@naver.com>
2022-01-31 12:24:34 +0900

    Initial commit

---
+++ src/main/java/com/example/jambe/JambeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jambe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JambeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JambeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jambe/JambeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jambe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JambeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

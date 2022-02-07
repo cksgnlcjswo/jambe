3d5bbfa942cf3bfc2d29dfabee0b84e5c77be856
찬 <cksgnlcjswoo@naver.com>
2022-02-08 01:11:20 +0900

    board view

--- src/main/java/com/example/jambe/controller/BoardApiController.java
+++ src/main/java/com/example/jambe/controller/BoardApiController.java
@@ -3,35 +3,40 @@ package com.example.jambe.controller;
 import com.example.jambe.domain.Board.Board;
 import com.example.jambe.dto.BoardDto;
 import com.example.jambe.service.BoardService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class BoardApiController {
 
     private final BoardService boardService;
 
-    @ResponseBody
+    //게시판 생성
     @PostMapping("/api/v1/board")
-    public String save(@RequestBody BoardDto boardDto) {
+    public String save(BoardDto boardDto) {
 
         boardService.save(boardDto);
-        return boardDto.getCategory() + "is created";
+        return "redirect:/";
+    }
+
+    @GetMapping("/board")
+    public String make() {
+        return "makeBoard";
     }
 
     //모든 종류의 게시판 보여주기
     @GetMapping("/api/v1/board")
-    public void allBoard(Model model) {
+    public String allBoard(Model model) {
 
         List<Board> boardList = boardService.findAll();
         model.addAttribute("Board",boardList);
+
+        return "board";
     }
 
     //종류별 게시판 입장

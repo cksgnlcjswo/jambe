05ea9fee886548d4ea9b793d61d1e4546c93035a
찬 <cksgnlcjswoo@naver.com>
2022-03-11 17:49:55 +0900

    session clustering & redis 연동

--- src/main/java/com/example/jambe/JambeApplication.java
+++ src/main/java/com/example/jambe/JambeApplication.java
@@ -2,7 +2,10 @@ package com.example.jambe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession
 @SpringBootApplication
 public class JambeApplication {
 

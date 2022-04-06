57a1be23ab448fab75c4c85dedd9122e0167d1ab
찬 <cksgnlcjswoo@naver.com>
2022-04-06 23:10:23 +0900

    real properties add

---
+++ src/test/java/com/example/jambe/controller/ProfileControllerUnitTest.java
@@ -0,0 +1,55 @@
+package com.example.jambe.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.context.annotation.Profile;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void profile_조회() {
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when (메소드 직접호출)
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void real이_없을때() {
+        //given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        String profile = controller.profile();
+
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void active_없을때() {
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        String profile = controller.profile();
+
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
